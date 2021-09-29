8be25c9032290efc2225015371c919f0554883cc
Aileen Pierce <aileen.pierce@colorado.edu>
2021-09-29 11:01:59 -0600

    multiple vcs

---
+++ week 06 iOS multiple VCs/favorites/favorites/Favorite.swift
@@ -0,0 +1,14 @@
+//
+//  Favorite.swift
+//  favorites
+//
+//  Created by Aileen Pierce
+//  Copyright (c) Aileen Pierce. All rights reserved.
+//
+
+import Foundation
+
+class Favorite {
+    var favBook : String?
+    var favAuthor : String?
+}
